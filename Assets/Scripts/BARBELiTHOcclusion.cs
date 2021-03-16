f51c1b1e9baa2905d4a8c7efd8ce9d2e160578a7
Niall Moody <21282294+NiallMoody@users.noreply.github.com>
2021-03-16 20:07:33 +0000

    Week 4 Niall: Implemented BARBELiTH occlusion.

---
+++ Assets/Scripts/BARBELiTHOcclusion.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BARBELiTHOcclusion : MonoBehaviour
+{
+	public Transform player;
+	public Transform barbelith;
+	public LibPdInstance barbelithPatch;
+
+	private float occlusion = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+		float occluded = 1.0f;
+
+		if(Physics.Raycast(player.position,
+						   barbelith.position - player.position,
+						   out hit,
+						   Mathf.Infinity))
+		{
+			if(hit.transform == barbelith)
+			{
+				occluded = 0.0f;
+			}
+		}
+
+		if(occlusion != occluded)
+		{
+			barbelithPatch.SendFloat("occlusion", occluded);
+
+			Debug.Log("Sent occlusion: " + occluded);
+
+			occlusion = occluded;
+		}
+	}
+}
---
+++ Assets/Scripts/BARBELiTHSizeWatcher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BARBELiTHSizeWatcher : MonoBehaviour
+{
+	public LibPdInstance barbelithPatch;
+	public Transform barbelithTransform;
+
+	private Vector3 lastSize;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        lastSize = barbelithTransform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(barbelithTransform.localScale != lastSize)
+		{
+			//1 -> 5 in first scene.
+			//(previously 50 -> 40).
+			barbelithPatch.SendFloat("size", barbelithTransform.localScale.x);
+
+			lastSize = barbelithTransform.localScale;
+		}
+	}
+}
