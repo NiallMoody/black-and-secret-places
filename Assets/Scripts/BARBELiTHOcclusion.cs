831022fe2ddad701d5e608be26c93631bfefe2c9
Niall Moody <21282294+NiallMoody@users.noreply.github.com>
2021-04-13 20:15:07 +0100

    Finished BARBELiTH occlusion, started on inverted occlusion whispers.

--- Assets/Scripts/BARBELiTHOcclusion.cs
+++ Assets/Scripts/BARBELiTHOcclusion.cs
@@ -6,7 +6,7 @@ public class BARBELiTHOcclusion : MonoBehaviour
 {
 	public Transform player;
 	public Transform barbelith;
-	public LibPdInstance barbelithPatch;
+	public LibPdInstance[] pdPatches;
 
 	private float occlusion = 0.0f;
 
@@ -15,8 +15,9 @@ public class BARBELiTHOcclusion : MonoBehaviour
     {
 		float occluded = 0.0f;
         RaycastHit hit;
+		Vector3 dir = (barbelith.position - player.position).normalized;
 
-		if(Physics.Raycast(player.position,
+		if (Physics.Raycast(player.position,
 						   barbelith.position - player.position,
 						   out hit,
 						   Mathf.Infinity))
@@ -24,22 +25,40 @@ public class BARBELiTHOcclusion : MonoBehaviour
 			if(hit.transform == barbelith)
 			{
 				occluded += 1.0f;
+				Debug.DrawLine(player.position, barbelith.position, Color.yellow);
 			}
+			else
+				Debug.DrawLine(player.position, hit.point, Color.blue);
 		}
+
+		float mult = 1.0f;
+
+		if(player.position.z > barbelith.position.z)
+			mult = -1.0f;
 
 		for(int i=0;i<16;++i)
 		{
-			Vector3 pos = barbelith.position;
+			float angle = ((float)i/16.0f) * 2.0f * Mathf.PI;
+			Vector3 pos = new Vector3(Mathf.Sin(angle), Mathf.Cos(angle), 0.0f);
+
+			pos *= barbelith.localScale.x * 0.49f;
+
+			pos = Quaternion.FromToRotation(Vector3.forward * mult, dir) * pos;
+
+			pos += barbelith.position;
 
 			if (Physics.Raycast(player.position,
-							    pos - player.position,
-							    out hit,
-							    Mathf.Infinity))
+							   pos - player.position,
+							   out hit,
+							   Mathf.Infinity))
 			{
-				if (hit.transform == barbelith)
+				if(hit.transform == barbelith)
 				{
 					occluded += 1.0f;
+					Debug.DrawLine(player.position, pos, Color.yellow);
 				}
+				else
+					Debug.DrawLine(player.position, hit.point, Color.blue);
 			}
 		}
 
@@ -48,9 +67,8 @@ public class BARBELiTHOcclusion : MonoBehaviour
 
 		if(occlusion != occluded)
 		{
-			barbelithPatch.SendFloat("occlusion", occluded);
-
-			Debug.Log("Sent occlusion: " + occluded);
+			for(int i=0;i<pdPatches.Length;++i)
+				pdPatches[i].SendFloat("occlusion", occluded);
 
 			occlusion = occluded;
 		}
